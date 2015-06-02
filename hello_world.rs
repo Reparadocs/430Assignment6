e12a798f3f4dbbbd3326ad76c2d9abfa3bc5f1c3
Rishab Hegde <reparadocs@gmail.com>
2015-06-02 13:03:11 -0700

    Actually working v1

--- hello_world.rs
+++ hello_world.rs
@@ -1,67 +1,34 @@
-trait ExprC {
-   fn interp(&self) -> i32;
+enum ExprC {
+   numC { n : i32 },
+   boolC { b : bool },
+   idC { s : String },
+   ifC { test : Box<ExprC>, then : Box<ExprC>, els : Box<ExprC> },
+   appC { fun : Box<ExprC>, arg : Vec<ExprC> },
+   binOpC { op : String, l : Box<ExprC>, r : Box<ExprC> },
+   lamC { args : Vec<ExprC>, body : Box<ExprC> }
 }
 
-struct numC {
-   n: i32
-}
-
-struct boolC {
-   b: bool
-}
-
-struct idC {
-   s: String
-}
-
-struct ifC<'a> {
-   test: &'a (ExprC + 'a), 
-   then: &'a (ExprC + 'a),
-   els: &'a (ExprC + 'a)
-}
-
-struct appC<'a> {
-   fun: &'a (ExprC + 'a),
-   arg: Vec<ExprC>
-}
-
-struct binOpC<'a> {
-   op: String,
-   l: &'a (ExprC + 'a),
-   r: &'a (ExprC + 'a)
-}
-
-struct lamC<'a> {
-   args: Vec<ExprC>,
-   body: &'a (ExprC + 'a)
-}
-
-impl ExprC for numC {
-   fn interp(&self) -> i32 {
-      self.n 
+fn interp_binop(op: String, l: Box<ExprC>, r: Box<ExprC>) -> i32 {
+   let left = interp(*l);
+   let right = interp(*r);
+   match op.as_ref() {
+      "+" => left + right,
+      _ => left - right
    }
 }
 
-impl<'a> ExprC for binOpC {
-   fn interp(&self) -> i32 {
-      let left = self.l.interp();
-      let right = self.r.interp();
-
-      match self.op {
-         "+" => left + right,
-         "-" => left - right,
-         "*" => left * right,
-         "/" => left / right,
-      }
+fn interp(e: ExprC) -> i32 {
+   match e {
+      ExprC::numC { n: n } => n,
+      ExprC::boolC { b: b} => 5,
+      ExprC::binOpC { op: op, l: l, r: r } => interp_binop(op, l, r),
+      _ => 10
    }
-}
-
-fn interp(e: &ExprC) {
-   println!("Num: {}", e.interp());
 }
 
 fn main() {
    println!("Hello World!");
-   let test_num = binOpC {op : "+", l : numC { n : 5 }, r : numC { n : 3} };
-   interp(&test_num);
+   //let test_num = ExprC::numC {n : 3};
+   let test_num = ExprC::binOpC {op : "+".to_string(), l : Box::new(ExprC::numC { n : 5 }), r : Box::new(ExprC::numC { n : 3}) };
+   println!("{}", interp(test_num));
 }
